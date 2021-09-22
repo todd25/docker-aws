2060450c8a09587c540e37cd04ca8b4d53c27f6b
Zhengtao Li <todd@Zhengtaos-MacBook-Pro.local>
2021-09-21 22:13:35 -0700

    add search

--- flask/app/views.py
+++ flask/app/views.py
@@ -1,38 +1,17 @@
 from app import app
-from flask import render_template, request
+from flask import render_template, request,redirect,url_for
 import os
 import pymongo
 from pymongo import MongoClient
 import time
 import urllib.request
+import run
 
-
-
-def connect(host='http://google.com'):
-    try:
-        urllib.request.urlopen(host) #Python 3.x
-        return True
-    except:
-        return False
-
-@app.route("/")
+@app.route("/", methods=["POST","GET"])
 def index():
-    
-    print( "connected" if connect() else "no internet!" )
-
-    client = pymongo.MongoClient("mongodb+srv://todd:O12345@cluster0.nloih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-    db = client["cext"]
-    collection = db["1"]
-    print(client)
-    results = collection.find({"name":"bill"})
-    for result in results:
-        print(result)
-
-    args = None
-    if request.args:
-        args = request.args
-        return render_template("index.html",args = args)
-    return render_template("index.html",args = args)
+    if request.method == "POST":
+        return url_lookup(request.form["search"]);
+    return render_template("index.html")
 
     # Use os.getenv("key") to get environment variables
     # app_name = os.getenv("APP_NAME")
@@ -41,3 +20,17 @@ def index():
     #     return f"Hello from {app_name} running in a Docker container behind Nginx!"
 
     # return "Hello from Flask"
+
+@app.route("/<url>", methods=["POST","GET"])
+def website(url):
+    if request.method == "POST":
+        return url_lookup(request.form["search"]);
+    return render_template("index.html", current_url = url)
+
+
+def url_lookup(target_url):
+    print(target_url)
+    result = run.collection.find_one({"url":target_url})
+    print(result)
+
+    return redirect(url_for("website",url=target_url))
--- flask/run.py
+++ flask/run.py
@@ -1,4 +1,21 @@
 from app import app
+import pymongo
+from pymongo import MongoClient
+
+client = pymongo.MongoClient("mongodb+srv://todd:O12345@cluster0.nloih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client["cext"]
+collection = db["1"]
+
+def connect(host='http://google.com'):
+    try:
+        urllib.request.urlopen(host) #Python 3.x
+        return True
+    except:
+        return False
 
 if __name__ == "__main__":
-    app.run()
+    print( "connected" if connect() else "no internet!" )
+
+    print(client)
+    
+    app.run(debug=True)
